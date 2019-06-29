51c0fdd12d2b09afcefdbfe605dbf11c52f53954
bbusacs478 <a2uso@tuta.io>
2019-06-29 10:18:03 -0700

    Add Config class

---
+++ src/main/java/other/Config.kt
@@ -0,0 +1,8 @@
+package other
+
+class Config {
+    companion object {
+        const val NODE: String = "https://rinkeby.infura.io:443"
+        const val CONTRACT_ADDRESS: String = "0xced17a0ae3ca557b5960d6386c7e877b09be3b89"
+    }
+}
