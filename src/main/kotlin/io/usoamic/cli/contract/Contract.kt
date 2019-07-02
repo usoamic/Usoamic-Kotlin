5fb30b9b7ee40b23b0443d2aa0f226357fd3c699
bbusacs478 <a2uso@tuta.io>
2019-07-02 11:42:43 -0700

    Add balanceOf method

--- src/main/kotlin/io/usoamic/cli/contract/Contract.kt
+++ src/main/kotlin/io/usoamic/cli/contract/Contract.kt
@@ -1,7 +1,25 @@
 package io.usoamic.cli.contract
 
 import io.usoamic.cli.Account
+import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.generated.Uint256
+import java.math.BigInteger
+import java.util.*
+import java.util.Collections
+import org.web3j.abi.datatypes.Function
+
 
 class Contract constructor(filename: String) : TransactionManager(filename) {
+    init {
 
+    }
+
+    fun balanceOf(topicId: BigInteger): BigInteger {
+        val function = Function(
+            "balanceOf",
+            listOf(Uint256(topicId)),
+            listOf(object: TypeReference<Uint256>() { })
+        )
+        return executeCallSingleValueReturn(function) as BigInteger
+    }
 }
