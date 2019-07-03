5eba9d724079b473a34126117d6fbd4bb15aaed7
bbusacs478 <a2uso@tuta.io>
2019-07-03 05:27:25 -0700

    Add method for adding ideas

--- src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
+++ src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
@@ -5,6 +5,7 @@ import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.generated.Uint256
 import java.math.BigInteger
 import org.web3j.abi.datatypes.Function
+import org.web3j.abi.datatypes.Utf8String
 import java.lang.Exception
 
 
@@ -22,5 +23,15 @@ class Usoamic constructor(filename: String) : TransactionManager(filename) {
         )
         val result = executeCallSingleValueReturn(function)
         return if(result == null) result else result as BigInteger
+    }
+
+    @Throws(Exception::class)
+    fun addIdea(password: String, description: String): String {
+        val function = Function(
+            "addIdea",
+            listOf(Utf8String(description)),
+            emptyList()
+        )
+        return executeTransaction(password, function)
     }
 }
