923e2ee63e46939f78bccea124378300b66478b2
bbusacs478 <a2uso@tuta.io>
2019-07-03 04:04:59 -0700

    Add custom MnemonicUtils with generateMnemonic function w/o argument

---
+++ src/main/kotlin/io/usoamic/cli/util/MnemonicUtils.kt
@@ -0,0 +1,15 @@
+package io.usoamic.cli.util
+
+import org.web3j.crypto.MnemonicUtils
+import java.security.SecureRandom
+
+open class MnemonicUtils : org.web3j.crypto.MnemonicUtils() {
+    companion object {
+        private val secureRandom = SecureRandom()
+        fun generateMnemonic(): String {
+            val initialEntropy = ByteArray(16)
+            secureRandom.nextBytes(initialEntropy)
+            return generateMnemonic(initialEntropy)
+        }
+    }
+}
