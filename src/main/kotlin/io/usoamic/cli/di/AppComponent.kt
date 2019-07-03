526e8f07a577d279b9548bd44220c789bd0bd411
bbusacs478 <a2uso@tuta.io>
2019-07-03 04:11:27 -0700

    Fix imports and class naming

--- src/main/kotlin/io/usoamic/cli/di/AppComponent.kt
+++ src/main/kotlin/io/usoamic/cli/di/AppComponent.kt
@@ -3,13 +3,15 @@ package io.usoamic.cli.di
 import io.usoamic.cli.contract.AccountWrapper
 import io.usoamic.cli.contract.TransactionManager
 import dagger.Component
+import io.usoamic.cli.UsoWalletCli
+import io.usoamic.cli.contract.Usoamic
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [ContractModule::class])
 interface AppComponent {
-    fun inject(clazz: io.usoamic.cli.contract.Usoamic)
+    fun inject(clazz: Usoamic)
     fun inject(clazz: AccountWrapper)
     fun inject(clazz: TransactionManager)
-    fun inject(clazz: Usoamic)
+    fun inject(clazz: UsoWalletCli)
 }
