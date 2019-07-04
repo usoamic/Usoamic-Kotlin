c2b83b3071c6a5fcff24224c0304dd524e79691f
bbusacs478 <a2uso@tuta.io>
2019-07-04 11:21:50 -0700

    Add method for getting ideas

--- src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
+++ src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
@@ -1,7 +1,10 @@
 package io.usoamic.cli.core
 
+import io.usoamic.cli.model.Idea
+import io.usoamic.cli.model.Vote
 import org.web3j.abi.TypeReference
 import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.Bool
 import org.web3j.abi.datatypes.generated.Uint256
 import java.math.BigInteger
 import org.web3j.abi.datatypes.Function
@@ -33,5 +36,34 @@ class Usoamic constructor(filename: String) : TransactionManager(filename) {
             emptyList()
         )
         return executeTransaction(password, function)
+    }
+
+    @Throws(Exception::class)
+    fun getIdea(ideaId: BigInteger): Idea {
+        val function = Function(
+            "getIdea",
+            listOf(Uint256(ideaId)),
+            listOf(
+                object: TypeReference<Bool>() { },
+                object: TypeReference<Uint256>() { },
+                object: TypeReference<Uint256>() { },
+                object: TypeReference<Address>() { },
+                object: TypeReference<Utf8String>() { },
+                object: TypeReference<Utf8String>() { }
+            )
+        )
+        val result = executeCall(function)
+        return Idea.Builder()
+            .setIsExist(result[0] as Boolean)
+            .setIdeaId(result[1] as BigInteger)
+            .setAuthor(result[2] as String)
+            .setDescription(result[3] as String)
+            .setIdeaStatus(result[4] as String)
+            .setTimestamp(result[5] as BigInteger)
+            .setNumberOfSupporters(result[6] as BigInteger)
+            .setNumberOfAbstained(result[7] as BigInteger)
+            .setNumberOfVotedAgainst(result[8] as BigInteger)
+            .setNumberOfParticipants(result[9] as BigInteger)
+            .build()
     }
 }
