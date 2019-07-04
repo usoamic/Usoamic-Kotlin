1792590f5d43ec236a63340e28b53534bc98df2b
bbusacs478 <a2uso@tuta.io>
2019-07-04 07:42:40 -0700

    Add method getPurchaseByAddress()

--- src/main/kotlin/io/usoamic/cli/core/Purchases.kt
+++ src/main/kotlin/io/usoamic/cli/core/Purchases.kt
@@ -1,6 +1,8 @@
 package io.usoamic.cli.core
 
+import io.usoamic.cli.model.Purchase
 import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Bool
 import org.web3j.abi.datatypes.Utf8String
 import org.web3j.abi.datatypes.generated.Uint256
@@ -28,5 +30,31 @@ open class Purchases constructor(filename: String) : TransactionManager(filename
         return executeTransaction(password, function)
     }
 
+    fun getPurchaseByAddress(address: String, id: BigInteger): Purchase {
+        val function = Function(
+            "getPurchaseByAddress",
+            listOf(
+                Address(address),
+                Uint256(id)
+            ),
+            listOf(
+                object : TypeReference<Bool>() {},
+                object : TypeReference<Uint256>() {},
+                object : TypeReference<Utf8String>() {},
+                object : TypeReference<Utf8String>() {},
+                object : TypeReference<Uint256>() {},
+                object : TypeReference<Uint256>() {}
+            )
+        )
+        val result = executeCall(function)
 
+        return Purchase.Builder()
+            .setIsExist(result[0] as Boolean)
+            .setId(result[1] as BigInteger)
+            .setPurchaseId(result[2] as String)
+            .setAppId(result[3] as String)
+            .setCost(result[4] as BigInteger)
+            .setTimestamp(result[5] as BigInteger)
+            .build()
+    }
 }
