3ef7a6b36102f9a5ac77fb8af089fb2b38906883
bbusacs478 <a2uso@tuta.io>
2019-07-04 05:51:00 -0700

    Move Idea-functions to Idea class

--- src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
+++ src/main/kotlin/io/usoamic/cli/core/Usoamic.kt
@@ -14,7 +14,7 @@ import org.web3j.abi.datatypes.generated.Uint8
 import java.lang.Exception
 
 
-class Usoamic constructor(filename: String) : TransactionManager(filename) {
+class Usoamic constructor(filename: String) : Ideas(filename) {
     init {
 
     }
@@ -28,79 +28,5 @@ class Usoamic constructor(filename: String) : TransactionManager(filename) {
         )
         val result = executeCallSingleValueReturn(function)
         return if(result == null) result else result as BigInteger
-    }
-
-    @Throws(Exception::class)
-    fun addIdea(password: String, description: String): String {
-        val function = Function(
-            "addIdea",
-            listOf(Utf8String(description)),
-            emptyList()
-        )
-        return executeTransaction(password, function)
-    }
-
-    @Throws(Exception::class)
-    fun getIdea(ideaId: BigInteger): Idea {
-        val function = Function(
-            "getIdea",
-            listOf(Uint256(ideaId)),
-            listOf(
-                object: TypeReference<Bool>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Address>() { },
-                object: TypeReference<Utf8String>() { },
-                object: TypeReference<Uint8>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Uint256>() { }
-            )
-        )
-        val result = executeCall(function)
-        val ideaStatusId = result[4].value as BigInteger
-
-        return Idea.Builder()
-            .setIsExist(result[0].value as Boolean)
-            .setIdeaId(result[1].value as BigInteger)
-            .setAuthor(result[2].value as String)
-            .setDescription(result[3].value as String)
-            .setIdeaStatus(IdeaStatus.values()[ideaStatusId.toInt()])
-            .setTimestamp(result[5].value as BigInteger)
-            .setNumberOfSupporters(result[6].value as BigInteger)
-            .setNumberOfAbstained(result[7].value as BigInteger)
-            .setNumberOfVotedAgainst(result[8].value as BigInteger)
-            .setNumberOfParticipants(result[9].value as BigInteger)
-            .build()
-    }
-
-    @Throws(Exception::class)
-    fun getVote(ideaId: BigInteger, voteId: BigInteger): Vote {
-        val function = Function(
-            "getVote",
-            listOf(
-                Uint256(ideaId),
-                Uint256(voteId)
-            ),
-            listOf(
-                object: TypeReference<Bool>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Uint256>() { },
-                object: TypeReference<Address>() { },
-                object: TypeReference<Utf8String>() { },
-                object: TypeReference<Utf8String>() { }
-            )
-        )
-        val result = executeCall(function)
-
-        return Vote.Builder()
-            .setIsExist(result[0].value as Boolean)
-            .setIdeaId(result[1].value as BigInteger)
-            .setVoteId(result[2].value as BigInteger)
-            .setVoter(result[3].value as String)
-            .setVoteType(result[4].value as String)
-            .setComment(result[5].value as String)
-            .build()
     }
 }
