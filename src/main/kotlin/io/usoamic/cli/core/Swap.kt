d95452751f159e463eeef2ac540c21b9501390bc
bbusacs478 <a2uso@tuta.io>
2019-07-04 07:58:23 -0700

    Add method for withdrawing ethers

--- src/main/kotlin/io/usoamic/cli/core/Swap.kt
+++ src/main/kotlin/io/usoamic/cli/core/Swap.kt
@@ -1,5 +1,19 @@
 package io.usoamic.cli.core
 
-open class Swap constructor(filename: String) : TransactionManager(filename) {
+import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.Bool
+import org.web3j.abi.datatypes.Function
+import org.web3j.abi.datatypes.Utf8String
+import org.web3j.abi.datatypes.generated.Uint256
+import java.math.BigInteger
 
+open class Swap constructor(filename: String) : TransactionManager(filename) {
+    fun withdrawEth(password: String, value: BigInteger): String {
+        val function = Function(
+            "withdrawEth",
+            listOf(Uint256(value)),
+            emptyList()
+        )
+        return executeTransaction(password, function)
+    }
 }
