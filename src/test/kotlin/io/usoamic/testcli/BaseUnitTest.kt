f838f08bc5bbb9326968258130ff183413420b99
bbusacs478 <a2uso@tuta.io>
2019-07-05 09:40:51 -0700

    Add private access modifier to buildDagger() and move prepareDagger() to block of companion object

--- src/test/kotlin/io/usoamic/testcli/BaseUnitTest.kt
+++ src/test/kotlin/io/usoamic/testcli/BaseUnitTest.kt
@@ -4,22 +4,22 @@ import io.usoamic.testcli.di.TestUsoamicModule
 import io.usoamic.testcli.di.DaggerTestAppComponent
 import io.usoamic.testcli.di.TestAppComponent
 import io.usoamic.testcli.di.TestWeb3jModule
-import org.junit.Before
+import org.junit.jupiter.api.BeforeAll
 
 class BaseUnitTest {
     companion object {
         var componentTest: TestAppComponent = buildDagger()
-        fun buildDagger(): TestAppComponent {
+        private fun buildDagger(): TestAppComponent {
             return DaggerTestAppComponent
                 .builder()
                 .testUsoamicModule(TestUsoamicModule())
                 .testWeb3jModule(TestWeb3jModule())
                 .build()
         }
-    }
 
-    @Before
-    fun prepareDagger() {
-        componentTest = buildDagger()
+        @BeforeAll
+        fun prepareDagger() {
+            componentTest = buildDagger()
+        }
     }
 }
