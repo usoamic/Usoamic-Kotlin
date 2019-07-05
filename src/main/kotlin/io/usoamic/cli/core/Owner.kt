29257f23b38d4c3c68c7a01ed600255bb629c3d1
bbusacs478 <a2uso@tuta.io>
2019-07-05 00:39:07 -0700

    Add setFronzen() method

--- src/main/kotlin/io/usoamic/cli/core/Owner.kt
+++ src/main/kotlin/io/usoamic/cli/core/Owner.kt
@@ -1,5 +1,16 @@
 package io.usoamic.cli.core
 
-open class Owner constructor(filename: String) : TransactionManager(filename) {
+import org.web3j.abi.datatypes.Bool
+import org.web3j.abi.datatypes.Function
 
+open class Owner constructor(filename: String) : TransactionManager(filename) {
+    @Throws(Exception::class)
+    fun setFronzen(password: String, frozen: Boolean): String {
+        val function = Function(
+            "setFronzen",
+            listOf(Bool(frozen)),
+            emptyList()
+        )
+        return executeTransaction(password, function)
+    }
 }
