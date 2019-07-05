aa4afa63a3c08e6169e9de6b1bd7e192ac37a41a
bbusacs478 <a2uso@tuta.io>
2019-07-05 06:13:14 -0700

    Add VERSION parameter

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -6,5 +6,6 @@ class TestConfig {
         const val ACCOUNT_FILENAME: String = "test_account.json"
         const val CONTRACT_ADDRESS: String = "0xced17a0ae3ca557b5960d6386c7e877b09be3b89"
         const val PASSWORD: String = "1234!"
+        const val VERSION: String = "v2"
     }
 }
