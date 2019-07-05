31eb15cf68827c96a6c839f87e2049d464d91929
bbusacs478 <a2uso@tuta.io>
2019-07-05 02:26:24 -0700

    Add @Singleton annotation

--- src/main/kotlin/io/usoamic/cli/di/UsoamicModule.kt
+++ src/main/kotlin/io/usoamic/cli/di/UsoamicModule.kt
@@ -4,10 +4,12 @@ import io.usoamic.cli.core.Usoamic
 import dagger.Module
 import dagger.Provides
 import io.usoamic.cli.other.Config
+import javax.inject.Singleton
 
 @Module
 class UsoamicModule {
     @Provides
+    @Singleton
     fun provideContract(): Usoamic {
         return Usoamic(Config.ACCOUNT_FILENAME)
     }
