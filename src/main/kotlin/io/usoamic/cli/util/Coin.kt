29643c443dab9ff7851efa71a0a86e3b99156264
bbusacs478 <a2uso@tuta.io>
2019-07-05 04:20:36 -0700

    Add fromCoin method with string argument

--- src/main/kotlin/io/usoamic/cli/util/Coin.kt
+++ src/main/kotlin/io/usoamic/cli/util/Coin.kt
@@ -6,6 +6,7 @@ import java.math.BigInteger
 class Coin private constructor(private val value: BigDecimal){
     companion object {
         fun fromSat(value: BigInteger): Coin = Coin(value.toBigDecimal(8).divide(getSatPerCoin()))
+        fun fromCoin(value: String): Coin = Coin(BigDecimal(value))
         fun fromCoin(value: BigDecimal): Coin = Coin(value)
         private fun getSatPerCoin(): BigDecimal = BigDecimal.TEN.pow(8).setScale(8)
     }
