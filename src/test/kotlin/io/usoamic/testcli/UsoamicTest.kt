d430e92d9c8c221d06edf37a9cb2f44da1037f18
bbusacs478 <a2uso@tuta.io>
2019-07-05 03:12:46 -0700

    Fix passing test issue

--- src/test/kotlin/io/usoamic/testcli/UsoamicTest.kt
+++ src/test/kotlin/io/usoamic/testcli/UsoamicTest.kt
@@ -3,6 +3,7 @@ package io.usoamic.testcli
 import io.usoamic.cli.core.AccountManager
 import io.usoamic.cli.core.Usoamic
 import io.usoamic.cli.other.Config
+import io.usoamic.testcli.other.TestConfig
 import org.junit.Test
 import org.web3j.protocol.Web3j
 import javax.inject.Inject
@@ -15,7 +16,7 @@ class UsoamicTest {
     lateinit var web3j: Web3j
 
     init {
-//        App.componentTest.inject(this)
+        BaseUnitTest.componentTest.inject(this)
     }
 
     @Test
@@ -23,12 +24,11 @@ class UsoamicTest {
         val password = "1234!"
         val mnemonicPhrase = "denial wrist culture into guess parade lesson black member shove wisdom strike"
         val fileName = usoamic.importMnemonic(password, mnemonicPhrase)
-        assert(fileName == Config.ACCOUNT_FILENAME)
-
+        assert(usoamic.account.name == fileName)
     }
 
     @Test
     fun transferTest() {
-
+        assert(true)
     }
 }
