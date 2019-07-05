71837c5f39313644192f007dd44f656615b46c3b
bbusacs478 <a2uso@tuta.io>
2019-07-05 08:46:16 -0700

    Add DEFAULT_ADDRESS variable

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -5,6 +5,7 @@ class TestConfig {
         const val NODE: String = "https://rinkeby.infura.io:443"
         const val ACCOUNT_FILENAME: String = "test_account.json"
         const val CONTRACT_ADDRESS: String = "0xced17a0ae3ca557b5960d6386c7e877b09be3b89"
+        const val DEFAULT_ADDRESS: String = "0x8b27fa2987630a1acd8d868ba84b2928de737bc2"
         const val PASSWORD: String = "1234!"
         const val VERSION: String = "v2"
     }
