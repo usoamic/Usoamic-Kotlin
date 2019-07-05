7bc0bea6600f54192813548f7d74f17938ed94ef
bbusacs478 <a2uso@tuta.io>
2019-07-05 03:19:22 -0700

    Add standart password

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -5,5 +5,6 @@ class TestConfig {
         const val NODE: String = "https://rinkeby.infura.io:443"
         const val ACCOUNT_FILENAME: String = "test_account.json"
         const val CONTRACT_ADDRESS: String = "0xced17a0ae3ca557b5960d6386c7e877b09be3b89"
+        const val PASSWORD: String = "1234!"
     }
 }
