411b37e17bd1d781fa1927b16f0766641ee74f13
bbusacs478 <a2uso@tuta.io>
2019-07-05 21:33:57 -0700

    Implement note testing

--- src/test/kotlin/io/usoamic/testcli/NotesTest.kt
+++ src/test/kotlin/io/usoamic/testcli/NotesTest.kt
@@ -1,9 +1,53 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.core.Usoamic
+import io.usoamic.cli.enum.NoteVisibility
+import io.usoamic.testcli.other.TestConfig
+import org.junit.jupiter.api.Test
 import javax.inject.Inject
+import kotlin.random.Random
+import kotlin.random.nextUInt
 
 class NotesTest {
     @Inject
     lateinit var usoamic: Usoamic
+
+    init {
+        BaseUnitTest.componentTest.inject(this)
+    }
+
+    @Test
+    fun addPublicNoteTest() {
+        addNoteTest(NoteVisibility.PUBLIC)
+    }
+
+    @Test
+    fun addUnlistedNoteTest() {
+        addNoteTest(NoteVisibility.UNLISTED)
+    }
+
+    private fun addNoteTest(noteVisibility: NoteVisibility) {
+        val content = generateNoteContent()
+        val addNoteTxHash = when (noteVisibility) {
+            NoteVisibility.PUBLIC -> usoamic.addPublicNote(TestConfig.PASSWORD, content)
+            NoteVisibility.UNLISTED -> usoamic.addUnlistedNote(TestConfig.PASSWORD, content)
+        }
+
+        usoamic.waitTransactionReceipt(addNoteTxHash) {
+            val author = usoamic.account.address
+            val noteId = usoamic.getLastNoteIdByAddress(author)
+            val note = usoamic.getNote(noteId!!)
+            assert(note.isExist)
+            assert(note.content == content)
+            assert(note.author == author)
+            assert(note.visibility == noteVisibility)
+
+            val commonNote = usoamic.getNoteByAddress(author, noteId)
+            assert(commonNote == note)
+        }
+    }
+
+    private fun generateNoteContent(): String {
+        return "Note #" + Random.nextInt()
+    }
 }
