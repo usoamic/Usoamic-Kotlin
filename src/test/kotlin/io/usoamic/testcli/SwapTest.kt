4a845fc7a180ece7c32932aaddf5f92e67e6c250
bbusacs478 <a2uso@tuta.io>
2019-07-05 23:34:46 -0700

    Add function for test ether deposit

--- src/test/kotlin/io/usoamic/testcli/SwapTest.kt
+++ src/test/kotlin/io/usoamic/testcli/SwapTest.kt
@@ -1,6 +1,10 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.core.Usoamic
+import io.usoamic.testcli.other.TestConfig
+import org.junit.jupiter.api.Test
+import org.web3j.utils.Convert
+import java.math.BigInteger
 import javax.inject.Inject
 
 class SwapTest {
@@ -9,5 +13,14 @@ class SwapTest {
 
     init {
         BaseUnitTest.componentTest.inject(this)
+    }
+
+    @Test
+    fun depositEthTest() {
+        val value = Convert.toWei("0.1", Convert.Unit.ETHER).toBigInteger()
+        val txHash = usoamic.transferEther(TestConfig.PASSWORD, TestConfig.CONTRACT_ADDRESS, value)
+        usoamic.waitTransactionReceipt(txHash) {
+            assert(it.status == "0x0")
+        }
     }
 }
