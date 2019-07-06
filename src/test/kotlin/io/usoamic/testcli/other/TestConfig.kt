494860b4af47608897964f7ee10096a883fa2b79
bbusacs478 <a2uso@tuta.io>
2019-07-05 23:15:58 -0700

    Add OWNER_ADDRESS const

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -6,6 +6,7 @@ class TestConfig {
         const val ACCOUNT_FILENAME: String = "test_account.json"
         const val CONTRACT_ADDRESS: String = "0xced17a0ae3ca557b5960d6386c7e877b09be3b89"
         const val DEFAULT_ADDRESS: String = "0x8b27fa2987630a1acd8d868ba84b2928de737bc2"
+        const val OWNER_ADDRESS: String = "0x5d8766ac0075bdf81b48f0bfcf92449e9def0f37"
         const val PASSWORD: String = "1234!"
         const val VERSION: String = "v2"
     }
