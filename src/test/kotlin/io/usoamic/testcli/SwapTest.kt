6eb98c7e6d609b6a8eebc3a24be6860e3b5cedfb
bbusacs478 <a2uso@tuta.io>
2019-07-05 23:56:35 -0700

    Add test for burnSwap() function

--- src/test/kotlin/io/usoamic/testcli/SwapTest.kt
+++ src/test/kotlin/io/usoamic/testcli/SwapTest.kt
@@ -1,6 +1,7 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.core.Usoamic
+import io.usoamic.cli.util.Coin
 import io.usoamic.testcli.other.TestConfig
 import org.junit.jupiter.api.Test
 import org.web3j.utils.Convert
@@ -21,6 +22,28 @@ class SwapTest {
         val txHash = usoamic.transferEther(TestConfig.PASSWORD, TestConfig.CONTRACT_ADDRESS, value)
         usoamic.waitTransactionReceipt(txHash) {
             assert(it.status == "0x0")
+        }
+    }
+
+    @Test
+    fun burnSwapTest() {
+        val value = Coin.ONE.toSat()
+
+        val address = usoamic.account.address
+
+        val accountBalance = usoamic.getBalance(address)
+        val accountTokenBalance = usoamic.balanceOf(address)
+        val contractBalance = usoamic.getBalance(TestConfig.CONTRACT_ADDRESS)
+
+        val txHash = usoamic.burnSwap(TestConfig.PASSWORD, value)
+        usoamic.waitTransactionReceipt(txHash) {
+            val newAccountBalance = usoamic.getBalance(address)
+            val newAccountTokenBalance = usoamic.balanceOf(address)
+            val newContractBalance = usoamic.getBalance(TestConfig.CONTRACT_ADDRESS)
+
+            assert(accountTokenBalance!!.subtract(newAccountTokenBalance).compareTo(value) == 0)
+            assert(newAccountBalance.subtract(accountBalance).compareTo(value) == 0)
+            assert(contractBalance.subtract(newContractBalance).compareTo(value) == 0)
         }
     }
 }
