2795bcf237fe5c5b2981d5e77850c0ccb81aacb6
bbusacs478 <a2uso@tuta.io>
2019-07-05 22:56:05 -0700

    Add tests for setFrozen() and setOwner()

--- src/test/kotlin/io/usoamic/testcli/OwnerTest.kt
+++ src/test/kotlin/io/usoamic/testcli/OwnerTest.kt
@@ -1,9 +1,34 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.core.Usoamic
+import io.usoamic.testcli.other.TestConfig
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.Keys
+import org.web3j.exceptions.MessageDecodingException
 import javax.inject.Inject
 
 class OwnerTest {
     @Inject
     lateinit var usoamic: Usoamic
+
+    init {
+        BaseUnitTest.componentTest.inject(this)
+    }
+
+    @Test
+    fun setFrozenTest() {
+        assertThrows<MessageDecodingException> {
+            usoamic.setFrozen(TestConfig.PASSWORD, true)
+        }
+    }
+
+    @Test
+    fun setOwnerTest() {
+        assertThrows<MessageDecodingException> {
+            val credentials = Credentials.create(Keys.createEcKeyPair())
+            usoamic.setOwner(TestConfig.PASSWORD, credentials.address)
+        }
+    }
 }
