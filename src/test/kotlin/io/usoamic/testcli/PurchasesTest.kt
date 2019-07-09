1c932a42e86d30fee237030364cdb01b2406b1d9
bbusacs478 <a2uso@tuta.io>
2019-07-09 09:55:52 -0700

    Code refactoring

--- src/test/kotlin/io/usoamic/testcli/PurchasesTest.kt
+++ src/test/kotlin/io/usoamic/testcli/PurchasesTest.kt
@@ -20,30 +20,31 @@ class PurchasesTest {
     fun makePurchaseTest() {
         val purchaser = usoamic.account.address
 
-        val ownerBalance = usoamic.balanceOf(TestConfig.OWNER_ADDRESS)
-        val purchaserBalance = usoamic.balanceOf(purchaser)
+        val ownerBalance = usoamic.balanceOf(TestConfig.OWNER_ADDRESS)!!
+        val purchaserBalance = usoamic.balanceOf(purchaser)!!
 
         val appId = "appId" + Random.nextInt()
         val purchaseId = "purchaseId" + Random.nextInt()
         val cost = Coin.ONE.toSat()
         val purchaseTxHash = usoamic.makePurchase(TestConfig.PASSWORD, appId, purchaseId,  cost)
-        val numberOfPurchases = usoamic.getNumberOfPurchaseByAddress(purchaser)
+        val numberOfPurchases = usoamic.getNumberOfPurchaseByAddress(purchaser)!!
 
         usoamic.waitTransactionReceipt(purchaseTxHash) {
-            val lastPurchaseId = usoamic.getLastPurchaseId(purchaser)
-            assert(numberOfPurchases!!.compareTo(lastPurchaseId) == 0)
+            val lastPurchaseId = usoamic.getLastPurchaseId(purchaser)!!
+            assert(numberOfPurchases.compareTo(lastPurchaseId) == 0)
 
-            val purchase = usoamic.getPurchaseByAddress(purchaser, lastPurchaseId!!)
+            val purchase = usoamic.getPurchaseByAddress(purchaser, lastPurchaseId)
             assert(purchase.isExist)
             assert(purchase.appId == appId)
             assert(purchase.id == lastPurchaseId)
             assert(purchase.purchaseId == purchaseId)
             assert(purchase.cost.compareTo(cost) == 0)
 
-            val newOwnerBalance = usoamic.balanceOf(TestConfig.OWNER_ADDRESS)
-            val newPurchaserBalance = usoamic.balanceOf(purchaser)
-            assert(newOwnerBalance!!.subtract(ownerBalance!!).compareTo(cost) == 0)
-            assert(purchaserBalance!!.subtract(newPurchaserBalance!!).compareTo(cost) == 0)
+            val newOwnerBalance = usoamic.balanceOf(TestConfig.OWNER_ADDRESS)!!
+            val newPurchaserBalance = usoamic.balanceOf(purchaser)!!
+
+            assert(newOwnerBalance.subtract(ownerBalance).compareTo(cost) == 0)
+            assert(purchaserBalance.subtract(newPurchaserBalance).compareTo(cost) == 0)
         }
     }
 
