104bd9ced5cc1b280beeb427d0a88aff24310b48
bbusacs478 <a2uso@tuta.io>
2019-07-20 00:04:09 -0700

    Update contract address

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -4,7 +4,7 @@ class TestConfig {
     companion object {
         const val NODE: String = "https://rinkeby.infura.io:443"
         const val ACCOUNT_FILENAME: String = "test_account.json"
-        const val CONTRACT_ADDRESS: String = "0x2068458d327dfb53a4c41287cac9be99decb0e4c"
+        const val CONTRACT_ADDRESS: String = "0x73ece1092f843c2d16d8902c40a585139a41da25"
         const val DEFAULT_ADDRESS: String = "0x8b27fa2987630a1acd8d868ba84b2928de737bc2"
         const val OWNER_ADDRESS: String = "0x5d8766ac0075bdf81b48f0bfcf92449e9def0f37"
         const val PASSWORD: String = "1234!"
