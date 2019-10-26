17df8f6e47754369d7e028612dc78a759ba72b6f
primys <a2uso@tuta.io>
2019-10-26 10:19:29 -0700

    Update contract addresses

--- src/main/kotlin/io/usoamic/usoamickt/other/Contract.kt
+++ src/main/kotlin/io/usoamic/usoamickt/other/Contract.kt
@@ -5,8 +5,8 @@ import io.usoamic.usoamickt.enum.NetworkType
 object Contract {
     fun forNetwork(networkType: NetworkType): String {
         return when(networkType) {
-            NetworkType.MAINNET -> "0x5e8a925ef8bceeb7d3fde6fedaeb374dcf39e69f"
-            NetworkType.TESTNET -> "0x5e8a925ef8bceeb7d3fde6fedaeb374dcf39e69f"
+            NetworkType.MAINNET -> "0x065E2FF284d89A16c84D6EEb3d825e9fA621A2b8"
+            NetworkType.TESTNET -> "0x1f559bB42f678D065fdeB71Ca3DE9d222C8aC756"
         }
     }
 }
