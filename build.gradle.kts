3477d4a6c9ee85a5de4c5669b3d17f463dd7f009
primys <a2uso@tuta.io>
2019-10-26 09:35:22 -0700

    Add dependency for fix dagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,13 @@ dependencies {
     compile("com.google.dagger", "dagger", "2.25.2")
     compile("com.google.code.gson", "gson", "2.8.5")
     compile("org.web3j", "core", "4.3.1")
+    compile("javax.annotation", "javax.annotation-api", "1.2")
     kapt("com.google.dagger", "dagger-compiler", "2.25.2")
 
     testCompile("org.jetbrains.kotlin", "kotlin-test-junit5", "1.3.50")
     testCompile("org.junit.jupiter", "junit-jupiter", "5.5.0")
+    testCompile("javax.annotation", "javax.annotation-api", "1.2")
+    kaptTest("com.google.dagger", "dagger-compiler", "2.25.2")
     testAnnotationProcessor("com.google.dagger", "dagger-compiler", "2.25.2")
 }
 
