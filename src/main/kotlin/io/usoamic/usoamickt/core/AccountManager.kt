3236feebed561c6669bf2d5627f625c5ca02b833
primys <a2uso@tuta.io>
2020-02-26 10:21:51 -0800

    Add custom wallet path support

--- src/main/kotlin/io/usoamic/usoamickt/core/AccountManager.kt
+++ src/main/kotlin/io/usoamic/usoamickt/core/AccountManager.kt
@@ -16,30 +16,30 @@ import java.io.IOException
 
 open class AccountManager(private val filename: String) {
     @Throws(InvalidPrivateKeyException::class, IOException::class)
-    fun importPrivateKey(password: String, privateKey: String): String {
+    fun importPrivateKey(password: String, privateKey: String, path: String = ""): String {
         if(!WalletUtils.isValidPrivateKey(privateKey)) {
             throw InvalidPrivateKeyException()
         }
         val credentials = Credentials.create(privateKey)
-        return import(password, credentials.ecKeyPair)
+        return import(password, credentials.ecKeyPair, path)
     }
 
     @Throws(InvalidMnemonicPhraseException::class, IOException::class)
-    fun importMnemonic(password: String, mnemonic: String): String {
+    fun importMnemonic(password: String, mnemonic: String, path: String = ""): String {
         if(!MnemonicUtils.validateMnemonic(mnemonic)) {
             throw InvalidMnemonicPhraseException()
         }
 
         val seed = MnemonicUtils.generateSeed(mnemonic, password)
         val keyPair = ECKeyPair.create(sha256(seed))
-        return import(password, keyPair)
+        return import(password, keyPair, path)
     }
 
     @Throws(Exception::class)
-    private fun import(password: String, keyPair: ECKeyPair): String {
+    private fun import(password: String, keyPair: ECKeyPair, path: String): String {
         val credentials = Credentials.create(keyPair)
 
-        val directory = File(WalletUtils.getDefaultKeyDirectory())
+        val directory = File(if(path.isEmpty()) WalletUtils.getDefaultKeyDirectory() else path)
         if(!directory.exists()) {
             directory.mkdir()
         }
