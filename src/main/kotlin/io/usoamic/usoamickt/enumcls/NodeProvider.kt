5f67c8173468ef337f4b98eeffcd24b299ba3082
primys <a2uso@tuta.io>
2020-04-24 10:48:31 -0700

    Added valueOf method to NodeProvider class

--- src/main/kotlin/io/usoamic/usoamickt/enumcls/NodeProvider.kt
+++ src/main/kotlin/io/usoamic/usoamickt/enumcls/NodeProvider.kt
@@ -4,4 +4,13 @@ sealed class NodeProvider {
     data class Infura(val projectId: String, val projectSecret: String = "") : NodeProvider()
     object MyEtherWallet : NodeProvider()
     object EtherScan : NodeProvider()
+
+    companion object {
+        fun valueOf(provider: String, args: String): NodeProvider = when (provider.toUpperCase()) {
+            "INFURA" -> Infura(args)
+            "MYETHERWALLET" -> MyEtherWallet
+            "ETHERSCAN" -> EtherScan
+            else -> throw ClassNotFoundException()
+        }
+    }
 }
