58e64c3aacf21cda3d7e04e1cb955b4f035b7cee
primys <a2uso@tuta.io>
2020-05-08 10:13:21 -0700

    Add file variable and initFile function

--- src/main/kotlin/io/usoamic/usoamickt/model/Account.kt
+++ src/main/kotlin/io/usoamic/usoamickt/model/Account.kt
@@ -2,6 +2,7 @@ package io.usoamic.usoamickt.model
 
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import java.io.File
 import java.math.BigInteger
 
 data class Account(
@@ -14,11 +15,18 @@ data class Account(
     @SerializedName("timestamp")
     val timestamp: BigInteger
 ) {
+    val file: File
+        get() = initFile(path, name)
+
     companion object {
         const val FILENAME: String = "account.json"
 
         fun fromJson(json: String): Account {
             return Gson().fromJson(json, Account::class.java)
         }
+
+        fun initFile(filePath: String, fileName: String): File = File(
+            if (filePath.isEmpty()) fileName else "$filePath${File.separator}$fileName"
+        )
     }
 }
