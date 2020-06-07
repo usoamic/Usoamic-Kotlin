99917fd88e6ae7d61283aaa8120a4a2864eb534f
primys <a2uso@tuta.io>
2020-06-07 02:56:49 -0700

    Fix account path

--- src/main/kotlin/io/usoamic/usoamickt/core/AccountManager.kt
+++ src/main/kotlin/io/usoamic/usoamickt/core/AccountManager.kt
@@ -16,37 +16,37 @@ import java.io.IOException
 
 open class AccountManager(private val fileName: String, private val filePath: String) {
     @Throws(InvalidPrivateKeyError::class, IOException::class)
-    fun importPrivateKey(password: String, privateKey: String, path: String = ""): String {
+    fun importPrivateKey(password: String, privateKey: String): String {
         if(!WalletUtils.isValidPrivateKey(privateKey)) {
             throw InvalidPrivateKeyError()
         }
         val credentials = Credentials.create(privateKey)
-        return import(password, credentials.ecKeyPair, path)
+        return import(password, credentials.ecKeyPair)
     }
 
     @Throws(InvalidMnemonicPhraseError::class, IOException::class)
-    fun importMnemonic(password: String, mnemonic: String, path: String = ""): String {
+    fun importMnemonic(password: String, mnemonic: String): String {
         if(!MnemonicUtils.validateMnemonic(mnemonic)) {
             throw InvalidMnemonicPhraseError()
         }
 
         val seed = MnemonicUtils.generateSeed(mnemonic, password)
         val keyPair = ECKeyPair.create(sha256(seed))
-        return import(password, keyPair, path)
+        return import(password, keyPair)
     }
 
     @Throws(Exception::class)
-    private fun import(password: String, keyPair: ECKeyPair, path: String): String {
+    private fun import(password: String, keyPair: ECKeyPair): String {
         val credentials = Credentials.create(keyPair)
 
-        val directory = File(if(path.isEmpty()) filePath else path)
+        val directory = File(filePath)
         if(!directory.exists()) {
             directory.mkdir()
         }
         val walletFileName = WalletUtils.generateWalletFile(password, keyPair, directory, false)
         val account = Account(credentials.address, directory.path, walletFileName, Timestamp.CURRENT)
 
-        FileWriter(if(path.isEmpty()) fileName else "$path${File.separator}$fileName").use {
+        FileWriter("$filePath${File.separator}$fileName").use {
             Gson().toJson(account, it)
         }
 
