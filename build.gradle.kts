b370eb88586a0f79f7bcf845c33b8ff2eee74017
primys <a2uso@tuta.io>
2020-09-26 20:16:35 -0700

    Change validateutilkt dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ object Version {
     const val dagger = "2.27"
     const val gson = "2.8.5"
     const val web3j = "4.6.0"
-    const val validateUtilKt = "16304cc35d"
+    const val validateUtilKt = "1.0.0"
     const val javaxAnnotationApi = "1.3.2"
     const val daggerCompiler = "2.27"
     const val kotlinTestJunit5 = "1.3.50"
