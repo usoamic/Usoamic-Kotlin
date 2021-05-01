c076f922234100c474beb619c9591b1acb2953da
primys <primys@usoamic.io>
2021-05-01 14:42:01 -0700

    Add isAndroid const to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Version {
+    private const val isAndroid = true
     const val dagger = "2.27"
     const val gson = "2.8.5"
-    const val web3j = "4.6.0"
+    val web3j get() = if(isAndroid) "4.6.0-android" else "4.6.0"
     const val validateUtilKt = "1.0.0"
     const val javaxAnnotationApi = "1.3.2"
     const val daggerCompiler = "2.27"
