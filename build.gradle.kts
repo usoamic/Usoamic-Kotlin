f89873539be642b3db3e300c118d7e2569cc2f3b
primys <primys@usoamic.io>
2021-05-01 14:40:46 -0700

    Revert api instead implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Version {
-    private const val isAndroid = true
     const val dagger = "2.27"
     const val gson = "2.8.5"
-    val web3j get() = if(isAndroid) "4.6.0-android" else "4.6.0"
+    const val web3j = "4.6.0"
     const val validateUtilKt = "1.0.0"
     const val javaxAnnotationApi = "1.3.2"
     const val daggerCompiler = "2.27"
-    const val kotlinTestJunit5 = "1.4.32"
+    const val kotlinTestJunit5 = "1.3.50"
     const val junitJupiter = "5.5.0"
 }
 
 plugins {
     java
     maven
-    kotlin("jvm") version "1.4.32"
-    kotlin("kapt") version "1.4.32"
+    kotlin("jvm") version "1.3.72"
+    kotlin("kapt") version "1.3.72"
 }
 
 allprojects {
     group = "io.usoamic"
-    version = "1.2.2"
+    version = "1.2.1"
 }
 
 repositories {
@@ -34,8 +33,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.google.dagger", "dagger", Version.dagger)
     implementation("com.google.code.gson", "gson", Version.gson)
-    implementation("org.web3j", "core", Version.web3j)
-    implementation("com.github.usoamic", "validateutilkt", Version.validateUtilKt)
+    api("org.web3j", "core", Version.web3j)
+    api("com.github.usoamic", "validateutilkt", Version.validateUtilKt)
     implementation("javax.annotation", "javax.annotation-api", Version.javaxAnnotationApi)
     kapt("com.google.dagger", "dagger-compiler", Version.daggerCompiler)
 
