b2dd241575381762366ad5d096112a3f0013d4f7
primys <primys@usoamic.io>
2021-08-01 14:18:43 +0300

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/io/usoamic/dependencies/Dependencies.kt
@@ -0,0 +1,35 @@
+package io.usoamic.dependencies
+
+object Dependencies {
+    object Usoamic {
+        const val validateUtilKt = "com.github.usoamic:validateutilkt:${Versions.Usoamic.validateUtilKt}"
+    }
+
+    object Java {
+        const val javaxAnnotationApi = "javax.annotation:javax.annotation-api:${Versions.Java.javaxAnnotationApi}"
+    }
+
+    object Kotlin {
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect"
+        const val stdLibJDK = "org.jetbrains.kotlin:kotlin-stdlib"
+        const val stdLibJDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    }
+
+    object DI {
+        const val dagger = "com.google.dagger:dagger:${Versions.DI.dagger}"
+        const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.DI.daggerCompiler}"
+    }
+
+    object Cryptocurrency {
+        val web3j get() = "org.web3j:core:${Versions.Cryptocurrency.web3j}"
+    }
+
+    object Test {
+        const val kotlinTestJunit = "org.jetbrains.kotlin:kotlin-test-junit5:${Versions.Test.kotlinTestJunit}"
+        const val junitJupiter = "org.junit.jupiter:junit-jupiter:${Versions.Test.junitJupiter}"
+    }
+
+    object Other {
+        const val gson = "com.google.code.gson:gson:${Versions.Other.gson}"
+    }
+}
---
+++ buildSrc/src/main/kotlin/io/usoamic/dependencies/Versions.kt
@@ -0,0 +1,31 @@
+package io.usoamic.dependencies
+
+object Versions {
+    private const val isAndroid = true
+
+    object Usoamic {
+        const val validateUtilKt = "1.0.1-3"
+    }
+
+    object Java {
+        const val javaxAnnotationApi = "1.3.2"
+    }
+
+    object DI {
+        const val dagger = "2.27"
+        const val daggerCompiler = "2.27"
+    }
+
+    object Cryptocurrency {
+        val web3j get() = if (isAndroid) "4.6.0-android" else "4.6.0"
+    }
+
+    object Test {
+        const val kotlinTestJunit = "1.5.20"
+        const val junitJupiter = "5.5.0"
+    }
+
+    object Other {
+        const val gson = "2.8.5"
+    }
+}
